273e8d80301e608fc1b093081cb8ad4470514378
ChaedongIm <chaedong.im.dev@gmail.com>
2023-06-21 19:39:40 +0900

    Solve: 6월 21일 문제 풀이 (#17)
    * Solve: 6월12~14일 문제 벼락치기
    
    * Solve: N과 M 9
    
    * Solve: N과M 11
    
    * Solve: BOJ6603_실2_로또

--- 임채동_문제풀이/4주차_문제풀이/BOJ15665_실2_N과M11.java
+++ 임채동_문제풀이/4주차_문제풀이/BOJ15665_실2_N과M11.java
@@ -5,12 +5,11 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-public class BOJ15663_실2_N과M9 {
+public class BOJ15665_실2_N과M11 {
     private static int n;
     private static int m;
+    private static int[] permutations;
     private static int[] inputs;
-    private static int[] permutation;
-    private static boolean[] check;
     private static Set<String> set = new LinkedHashSet<>();
 
     public static void main(String[] args) throws IOException {
@@ -18,15 +17,13 @@ public class BOJ15663_실2_N과M9 {
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-        st = new StringTokenizer(br.readLine());
+        permutations = new int[m + 1];
         inputs = new int[n + 1];
+        st = new StringTokenizer(br.readLine());
         for (int i = 1; i <= n; i++) {
             inputs[i] = Integer.parseInt(st.nextToken());
         }
         Arrays.sort(inputs);
-
-        permutation = new int[m + 1];
-        check = new boolean[n + 1];
 
         dfs(1);
         printAnswer();
@@ -41,22 +38,17 @@ public class BOJ15663_실2_N과M9 {
 
     private static void dfs(int depth) {
         if (depth == m + 1) {
-            StringBuilder temp = new StringBuilder();
+            StringBuilder sb = new StringBuilder();
             for (int i = 1; i <= m; i++) {
-                temp.append(permutation[i] + " ");
+                sb.append(permutations[i] + " ");
             }
-
-            set.add(temp.toString());
+            set.add(sb.toString());
             return;
         }
 
         for (int i = 1; i <= n; i++) {
-            if (!check[i]) {
-                check[i] = true;
-                permutation[depth] = inputs[i];
-                dfs(depth + 1);
-                check[i] = false;
-            }
+            permutations[depth] = inputs[i];
+            dfs(depth + 1);
         }
     }
 }
