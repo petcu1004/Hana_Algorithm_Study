eb5f000e2d53cb3aa81a38ec0779223c2eda9d9e
ChaedongIm <chaedong.im.dev@gmail.com>
2023-06-26 08:11:08 +0900

    [06.26] 발표용 코멘트 추가 (#29)
    * Solve: 6월12~14일 문제 벼락치기
    
    * Solve: N과 M 9
    
    * Solve: N과M 11
    
    * Solve: BOJ6603_실2_로또
    
    * Solve: 암호만들기
    
    * Refactor: 날짜기준으로 이름 변경
    
    * 2023.06.21(수) 코테 2문제 풀이
    
    * 2023.06.23(금) 까지의 문제 풀이 업로드
    
    * Merge branch 'main' of https://github.com/YOOBINNOH/Hana_Algorithm_Study
    
    * 2023.06.26(월) 스터디 발표 준비
    
    * Chore: PR Template 작성

---
+++ pull_request_template.md
@@ -0,0 +1,3 @@
+#### PR Name Convention
+
+ - ex) [06.25] 경로 탐색, 누적 합 구하기
--- 임채동_문제풀이/5주차_문제풀이/230619_실3_퇴사_발표.java
+++ 임채동_문제풀이/5주차_문제풀이/230619_실3_퇴사_발표.java
@@ -6,6 +6,58 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
+public class BOJ14501_실3_퇴사 {
+    private static int n;
+    private static StringTokenizer st;
+    private static int[] times;
+    private static int[] prices;
+    private static boolean[] check;
+    private static int max = Integer.MIN_VALUE;
+
+
+    public static void main(String[] args) throws IOException {
+        setInput();
+        // 어디부터 시작해야 좋은지 모르니까 일단 다 확인해야할것같은데??
+        for (int i = 0; i < n; i++) {
+            dfs(0, 0, 0);
+        }
+        System.out.println(max);
+    }
+    // 끝났는지 아닌지 언제 끝났는지 이런거 확ㅇㄴ할수가없네 
+    // depth == n으로는 안된다. 
+
+    private static void dfs(int depth, int start, int sum) {
+        for (int i = start; i < n; i++) {
+            // 만약 퇴사날전까지 끝이 난다면,
+            if (i + times[i] <= n) {
+                dfs(depth + 1, i + times[i], sum + prices[i]);
+            }
+        }
+        // baseCase 없이 그냥 최대값을 찍으면 된다.
+        max = Math.max(max, sum);
+    }
+
+    private static void setInput() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        times = new int[n];
+        prices = new int[n];
+        check = new boolean[n];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            times[i] = Integer.parseInt(st.nextToken());
+            prices[i] = Integer.parseInt(st.nextToken());
+        }
+    }
+}
+
+/**
+ * 1. 백트래킹 조건은 현재 시작점에서 소요 날을 더했을때 퇴사날 이전이어야 하는점
+ * 2. 베이스 케이스는 최초엔 depth == n으로 생각했으나 depth가 끝까지 못가는 경우가 대부분이기에
+ *    베이스 케이스 없이 모든 경우에 max를 구해주도록 했다.
+ * 3. dfs의 시작점을 무조건 첫날로 하는것이 아니라 다음날부터 시작할수도있도록 처음부터 끝날까지 
+ *    시작점으로 잡아서 max를 구해보도록 완전 탐색했다.
+ */
 
 //! 틀림
 // public class BOJ14501_실3_퇴사 {
