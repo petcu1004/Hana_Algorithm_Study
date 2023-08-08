8c2f927e99dee2bdb6f11d53a697cc8b510d48e8
petcu1004 <kty4585@naver.com>
2023-08-09 01:32:25 +0900

    [08.08] 가장 큰 증가하는 부분 수열, 스티커

---
+++ 민새미/[08.08] 가장 큰 증가하는 부분 수열.py
@@ -0,0 +1,15 @@
+n=int(input())
+
+arr=list(map(int, input().split()))
+
+dp=[1 for _ in range(n)]
+dp[0]=arr[0]
+for i in range(n):
+    for j in range(i):
+        if arr[i]>arr[j]:
+            dp[i]=max(dp[i], dp[j]+arr[i])
+        else:
+            dp[i]=max(dp[i], arr[i])
+
+# print(dp)
+print(max(dp))
