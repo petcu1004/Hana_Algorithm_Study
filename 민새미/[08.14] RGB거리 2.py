6abb7e5b06f8c1abe3086f80eb6d18e9f78bf4f0
petcu1004 <kty4585@naver.com>
2023-08-16 02:27:13 +0900

    [08.14] 주석 추가

--- 민새미/[08.14] RGB거리 2.py
+++ 민새미/[08.14] RGB거리 2.py
@@ -4,7 +4,7 @@ arr=[list(map(int, input().split()))for _ in range(n)]
 
 ans=E=1e9
 for i in range(3):
-    dp=[[E, E, E] for _ in range(n)]
+    dp=[[E, E, E] for _ in range(n)] # dp가 각 R, G, B로 시작했을 때
     dp[0][i]=arr[0][i] #처음 집 색칠
     for j in range(1, n): #2번째 집부터 R, G, B로 색칠했을 때 최소값 갱신
         dp[j][0]=arr[j][0]+min(dp[j-1][1], dp[j-1][2])
